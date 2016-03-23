4e16dd2e82620f4b044ee587176fe29033daaa35
Jeff LaMarche <jeff@Jeffs-Mac-Pro-2.local>
2016-03-23 13:23:24 -0400

    Added support for multiple stroke colors and line widths in one signature, updated sample app to use this functionality

--- SignatureTest/SignatureTest/ViewController.swift
+++ SignatureTest/SignatureTest/ViewController.swift
@@ -9,37 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	// Connect this Outlet to the Signature View
+	@IBOutlet weak var drawSignatureView: YPDrawSignatureView!
+	@IBOutlet weak var blackSwatch: UIButton!
+	@IBOutlet weak var blueSwatch: UIButton!
+	@IBOutlet weak var redSwatch: UIButton!
+	@IBOutlet weak var greenSwatch: UIButton!
+	@IBOutlet weak var widthOne: UIButton!
+	@IBOutlet weak var widthTwo: UIButton!
+	@IBOutlet weak var widthThree: UIButton!
+	@IBOutlet weak var widthFour: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	
+	@IBAction func clearSignature(sender: UIButton) {
+		self.drawSignatureView.clearSignature()
+	}
+	
+	@IBAction func saveSignature(sender: UIButton) {
+		let signatureImage = self.drawSignatureView.getSignature()
+		
+		UIImageWriteToSavedPhotosAlbum(signatureImage, nil, nil, nil)
 
-  // Connect this Outlet to the Signature View
-  @IBOutlet weak var drawSignatureView: YPDrawSignatureView!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func clearSignature(sender: UIButton) {
-    // This is how the signature gets cleared
-    self.drawSignatureView.clearSignature()
-  }
-  
-  @IBAction func saveSignature(sender: UIButton) {
-    // Getting the Signature Image from self.drawSignatureView using the method getSignature().
-    let signatureImage = self.drawSignatureView.getSignature()
-    
-    // Saving signatureImage from the line above to the Photo Roll.
-    // The first time you do this, the app asks for access to your pictures.
-    UIImageWriteToSavedPhotosAlbum(signatureImage, nil, nil, nil)
-    
-    // Since the Signature is now saved to the Photo Roll, the View can be cleared anyway.
-    self.drawSignatureView.clearSignature()
-  }
-  
-
+		self.drawSignatureView.clearSignature()
+	}
+	
+	@IBAction func setPenColor(sender: UIButton) {
+		if (sender == blueSwatch) {
+			drawSignatureView.strokeColor = UIColor(red: 0.058823529, green: 0.02745098, blue: 0.509803922, alpha: 1.0)
+		} else if (sender == redSwatch) {
+			drawSignatureView.strokeColor = UIColor(red: 0.71372549, green: 0.0, blue: 0.0, alpha: 1.0)
+		} else if (sender == greenSwatch) {
+			drawSignatureView.strokeColor = UIColor(red: 0.015686275, green: 0.482352941, blue: 0.090196078, alpha: 1.0)
+		} else {
+			drawSignatureView.strokeColor = UIColor.blackColor()
+		}
+		drawSignatureView.setNeedsDisplay()
+		
+	}
+	
+	@IBAction func setLineWidth(sender: UIButton) {
+		if (sender == widthOne) {
+			drawSignatureView.lineWidth = 1.0
+		} else if (sender == widthTwo) {
+			drawSignatureView.lineWidth = 2.0
+		} else if (sender == widthThree) {
+			drawSignatureView.lineWidth = 3.0
+		} else if (sender == widthFour) {
+			drawSignatureView.lineWidth = 4.0
+		}
+	}
 }
 
--- SignatureTest/SignatureTest/YPDrawSignatureView.swift
+++ SignatureTest/SignatureTest/YPDrawSignatureView.swift
@@ -7,90 +7,105 @@
 //
 
 import UIKit
+
+public struct StrokeSegment {
+	public var path: UIBezierPath!
+	public var color: UIColor!
+	init(path: UIBezierPath, color: UIColor, lineWidth: CGFloat) {
+		self.path = path
+		self.color = color
+		self.path.lineWidth = lineWidth
+	}
+}
+
+
 public class YPDrawSignatureView: UIView {
-  
-  // MARK: - Public properties
-  public var lineWidth: CGFloat = 2.0 {
-    didSet {
-      self.path.lineWidth = lineWidth
-    }
-  }
-  public var strokeColor: UIColor = UIColor.blackColor()
-  public var signatureBackgroundColor: UIColor = UIColor.whiteColor()
-  
-  // MARK: - Private properties
-  private var path = UIBezierPath()
-  private var pts = [CGPoint](count: 5, repeatedValue: CGPoint())
-  private var ctr = 0
-  
-  // MARK: - Init
-  required public init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    
-    self.backgroundColor = self.signatureBackgroundColor
-    self.path.lineWidth = self.lineWidth
-  }
-  
-  // MARK: - Draw
-  override public func drawRect(rect: CGRect) {
-    self.strokeColor.setStroke()
-    self.path.stroke()
-  }
-  
-  // MARK: - Touch handling functions
-  override public func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    if let firstTouch = touches.first{
-      let touchPoint = firstTouch.locationInView(self)
-      self.ctr = 0
-      self.pts[0] = touchPoint
-    }
-  }
-  
-  override public func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    if let firstTouch = touches.first{
-      let touchPoint = firstTouch.locationInView(self)
-      self.ctr++
-      self.pts[self.ctr] = touchPoint
-      if (self.ctr == 4) {
-        self.pts[3] = CGPointMake((self.pts[2].x + self.pts[4].x)/2.0, (self.pts[2].y + self.pts[4].y)/2.0)
-        self.path.moveToPoint(self.pts[0])
-        self.path.addCurveToPoint(self.pts[3], controlPoint1:self.pts[1], controlPoint2:self.pts[2])
-        
-        self.setNeedsDisplay()
-        self.pts[0] = self.pts[3]
-        self.pts[1] = self.pts[4]
-        self.ctr = 1
-      }
-      
-      self.setNeedsDisplay()
-    }
-  }
-  
-  override public func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    if self.ctr == 0{
-      let touchPoint = self.pts[0]
-      self.path.moveToPoint(CGPointMake(touchPoint.x-1.0,touchPoint.y))
-      self.path.addLineToPoint(CGPointMake(touchPoint.x+1.0,touchPoint.y))
-      self.setNeedsDisplay()
-    } else {
-      self.ctr = 0
-    }
-  }
-  
-  // MARK: - Helpers
-  
-  // MARK: Clear the Signature View
-    public func clearSignature() {
-      self.path.removeAllPoints()
-      self.setNeedsDisplay()
-  }
-  
-  // MARK: Save the Signature as an UIImage
-  public func getSignature() ->UIImage {
-    UIGraphicsBeginImageContext(CGSizeMake(self.bounds.size.width, self.bounds.size.height))
-    self.layer.renderInContext(UIGraphicsGetCurrentContext()!)
-    let signature: UIImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    return signature
-  }
+	
+	// MARK: - Public properties
+	public var lineWidth: CGFloat = 2.0
+	public var strokeColor: UIColor = UIColor.blackColor()
+	public var signatureBackgroundColor: UIColor = UIColor.whiteColor()
+	
+	// MARK: - Private properties
+	private var segments: [StrokeSegment] = []
+	private var currentSegment: StrokeSegment?
+	
+	private var points = [CGPoint](count: 5, repeatedValue: CGPoint())
+	private var pointCounter = 0
+	
+	// MARK: - Init
+	required public init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		self.backgroundColor = self.signatureBackgroundColor
+	}
+	
+	// MARK: - Draw
+	override public func drawRect(rect: CGRect) {
+		
+		for oneSegment in segments {
+			oneSegment.color.setStroke()
+			oneSegment.path.stroke()
+		}
+	}
+	
+	// MARK: - Touch handling functions
+	override public func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		if let firstTouch = touches.first{
+			let touchPoint = firstTouch.locationInView(self)
+			self.pointCounter = 0
+			self.points[0] = touchPoint
+			
+			self.currentSegment = StrokeSegment(path: UIBezierPath(), color: self.strokeColor, lineWidth:self.lineWidth)
+			segments.append(self.currentSegment!)
+		}
+	}
+	
+	override public func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		if let firstTouch = touches.first{
+			let touchPoint = firstTouch.locationInView(self)
+	  self.pointCounter += 1
+			self.points[self.pointCounter] = touchPoint
+			if (self.pointCounter == 4) {
+				self.points[3] = CGPointMake((self.points[2].x + self.points[4].x)/2.0, (self.points[2].y + self.points[4].y)/2.0)
+				self.segments.last!.path.moveToPoint(self.points[0])
+				self.segments.last!.path.addCurveToPoint(self.points[3], controlPoint1:self.points[1], controlPoint2:self.points[2])
+				
+				self.setNeedsDisplay()
+				self.points[0] = self.points[3]
+				self.points[1] = self.points[4]
+				self.pointCounter = 1
+			}
+			
+			self.setNeedsDisplay()
+		}
+	}
+	
+	override public func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		if self.pointCounter == 0{
+			let touchPoint = self.points[0]
+			self.segments.last!.path.moveToPoint(CGPointMake(touchPoint.x-1.0,touchPoint.y))
+			self.segments.last!.path.addLineToPoint(CGPointMake(touchPoint.x+1.0,touchPoint.y))
+			self.setNeedsDisplay()
+		} else {
+			self.pointCounter = 0
+		}
+	}
+	
+	// MARK: - Helpers
+	
+	// MARK: Clear the Signature View
+	public func clearSignature() {
+		self.segments.removeAll()
+		self.setNeedsDisplay()
+	}
+	
+	// MARK: Save the Signature as an UIImage
+	public func getSignature() ->UIImage {
+		UIGraphicsBeginImageContext(CGSizeMake(self.bounds.size.width, self.bounds.size.height))
+		self.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+		let signature: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		return signature
+	}
 }
