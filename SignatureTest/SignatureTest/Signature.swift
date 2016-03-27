1179a4c9e1bf212310f37c5835dd4183db00a0d2
Jeff LaMarche <jeff@jeffs-mac-pro-2.local>
2016-03-27 14:01:09 -0400

    Signatures now stored in separate object independent of view size or aspect ratio. Allows easier use with size classes where the signature view could potentially change due to rotation or split-view use.

---
+++ SignatureTest/SignatureTest/Signature.swift
@@ -0,0 +1,205 @@
+//
+//  Signature.swift
+//  SignatureTest
+//
+//  Created by Jeff LaMarche on 3/27/16.
+//  Copyright © 2016 Jeff LaMarche. All rights reserved.
+
+/** This class stores a signature captured in a draw signature view, but it stores the signature independent of the size or aspect ratio of the view it was drawn into
+
+*/
+import CoreGraphics
+	import UIKit
+
+public extension CGPoint {
+	static func midPointForPoints(p1: CGPoint, p2: CGPoint) -> CGPoint {
+		return CGPointMake((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+	}
+	static func controlPointForPoints(p1: CGPoint, p2: CGPoint) -> CGPoint {
+		var controlPoint = midPointForPoints(p1, p2: p2)
+		let diffY = abs(p2.y - controlPoint.y)
+		if (p1.y < p2.y) {
+			controlPoint.y += diffY
+		} else if (p1.y > p2.y) {
+			controlPoint.y -= diffY
+		}
+		return controlPoint
+	}
+}
+
+public struct StrokeSegment {
+	public var path: UIBezierPath!
+	public var color: UIColor!
+	init(path: UIBezierPath, color: UIColor, lineWidth: CGFloat) {
+		self.path = path
+		self.color = color
+		self.path.lineWidth = lineWidth
+	}
+}
+
+
+
+public struct SignatureSegment {
+	public var points = [CGPoint]()
+	public var lineWidth:CGFloat = 0.0
+	private var minX: CGFloat = 0.5
+	private var minY: CGFloat = 0.5
+	private var maxX: CGFloat = 0.5
+	private var maxY: CGFloat = 0.5
+	
+	public var color: UIColor!
+	init(color: UIColor, lineWidth: CGFloat) {
+		self.points = [CGPoint]()
+		self.color = color
+		self.lineWidth = lineWidth
+	}
+
+	/** Returns the signature's data converted for a new view size
+	@param viewSize The size of the view the signature is to be drawn in
+	@return array of array of CGPoints representing each of the pen strokes making up this signature, converted to the specified size
+	*/
+	func pointsForViewSized(viewSize: CGSize) -> [CGPoint]{
+		let viewAspectRatio = viewSize.width / viewSize.height
+		//let signatureAspectRatio = (maxX - minX) / (maxY - minY)
+		
+		// TODO: Check signature aspect ratio and make sure that it can be fully drawn in the specified view without loss
+		var convertedPoints = [CGPoint]()
+		
+		for point in self.points {
+			
+			if (viewAspectRatio > 1.0) { // landscape
+				let padding = (1.0 - ((viewSize.height / viewSize.width))) / 2.0
+				
+				let convertedPoint = CGPointMake(point.x * viewSize.width, (point.y - padding) * viewSize.height )
+				convertedPoints.append(convertedPoint)
+				
+			} else if (viewAspectRatio < 1.0) { // portrait
+				let padding = (1.0 - ((viewSize.width / viewSize.height))) / 2.0
+				let convertedPoint = CGPointMake((point.x - padding) * viewSize.width, point.y * viewSize.height )
+				convertedPoints.append(convertedPoint)
+				
+			} else { //square, no conversion necessary, just multiply clamp value by size
+				convertedPoints.append(CGPointMake(point.x * viewSize.width, point.y * viewSize.height))
+			}
+		}
+		
+		return convertedPoints
+	}
+	
+	
+}
+
+public class Signature {
+	
+	private var segments = [SignatureSegment]()
+	private var currentSegment: SignatureSegment?
+	
+	init() {
+	}
+	
+	
+	func startNewSegment(color: UIColor, lineWidth: CGFloat) {
+		segments.append(SignatureSegment(color: color, lineWidth: lineWidth))
+		currentSegment = segments.last
+	}
+	
+	func updateMinMax(point:CGPoint) {
+		if (point.x > currentSegment!.maxX) {
+			currentSegment!.maxX = point.x
+		} else if (point.x < currentSegment!.minX) {
+			currentSegment!.minX = point.x
+		}
+		if (point.y > currentSegment!.maxY) {
+			currentSegment!.maxY = point.y
+		} else if (point.y < currentSegment!.minY) {
+			currentSegment!.minY = point.y
+		}
+		
+		// Even though view can capture outside its bounds, we'll clamp to what was drawn inside the view
+		currentSegment!.maxX = min(currentSegment!.maxX, 1.0)
+		currentSegment!.maxY = min(currentSegment!.maxY, 1.0)
+		currentSegment!.minX = max(currentSegment!.minX, 0.0)
+		currentSegment!.minY = max(currentSegment!.minY, 0.0)
+		
+	}
+	func pathsForViewOfSize(size:CGSize, smoothed:Bool = true) -> [StrokeSegment] {
+		var ret = [StrokeSegment]()
+		
+		for oneSegment in segments {
+	
+			let points = oneSegment.pointsForViewSized(size)
+			
+			if (points.count == 0) {
+				continue
+			}
+			
+			let path = UIBezierPath()
+			path.moveToPoint(points[0])
+			
+			// less than 4 points, smoothing is impossible
+			if (points.count < 4 || !smoothed) { // also use this conditional if you don't want smoothing
+				for point in points {
+					path.addLineToPoint(point)
+				}
+			} else {
+				var p1 = points[0]
+	
+				for i in 1...(points.count - 1) {
+					let p2 = points[i]
+					let midPoint = CGPoint.midPointForPoints(p1, p2: p2)
+					if (i == 1) {
+						path.addLineToPoint(midPoint)
+					} else {
+						path.addQuadCurveToPoint(midPoint, controlPoint: p1)
+					}
+					
+					p1 = p2
+				}
+			}
+			ret.append(StrokeSegment(path: path, color: oneSegment.color, lineWidth: oneSegment.lineWidth))
+		}
+
+		
+		return ret
+	}
+	func addPoint(point:CGPoint, sourceViewSize:CGSize) {
+		
+		
+		if (sourceViewSize.width == 0 || sourceViewSize.height == 0) {
+			return
+		}
+		// If caller forgot to start a segment, we'll do it
+		if (segments.count == 0) {
+			startNewSegment(UIColor.blackColor(), lineWidth:2.0)
+		}
+		
+		let aspectRatio = sourceViewSize.width / sourceViewSize.height
+		
+		// If aspect ratio is not 1.0 (square), then we need to pad the signature into our square 0.0 - 1.0 coordinate space
+		if (aspectRatio > 1.0) { // Landscape
+			let padding = (1.0 - ((sourceViewSize.height / sourceViewSize.width))) / 2.0
+			let convertedPoint = CGPointMake(point.x / sourceViewSize.width, (point.y / sourceViewSize.height) + padding)
+			
+			updateMinMax(convertedPoint)
+			currentSegment!.points.append(convertedPoint)
+		} else if (aspectRatio < 1.0) { // portrait
+			let padding = (1.0 - ((sourceViewSize.width / sourceViewSize.height))) / 2.0
+			let convertedPoint = CGPointMake((point.x / sourceViewSize.width) + padding, point.y / sourceViewSize.height)
+			updateMinMax(convertedPoint)
+			currentSegment!.points.append(convertedPoint)
+		} else { // square
+			currentSegment!.points.append(CGPointMake(point.x / sourceViewSize.width, point.y / sourceViewSize.height))
+		}
+		print ("Point: \(point) stored as \(currentSegment!.points.last!)")
+		
+		
+		segments.removeLast()
+		segments.append(currentSegment!)
+	}
+	
+	func clearSignature() {
+		segments.removeAll()
+	}
+	
+	
+}
--- SignatureTest/SignatureTest/YPDrawSignatureView.swift
+++ SignatureTest/SignatureTest/YPDrawSignatureView.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-public struct StrokeSegment {
-	public var path: UIBezierPath!
-	public var color: UIColor!
-	init(path: UIBezierPath, color: UIColor, lineWidth: CGFloat) {
-		self.path = path
-		self.color = color
-		self.path.lineWidth = lineWidth
-	}
-}
-
+//public struct StrokeSegment {
+//	public var path: UIBezierPath!
+//	public var color: UIColor!
+//	init(path: UIBezierPath, color: UIColor, lineWidth: CGFloat) {
+//		self.path = path
+//		self.color = color
+//		self.path.lineWidth = lineWidth
+//	}
+//}
 
 public class YPDrawSignatureView: UIView {
 	
@@ -27,11 +26,11 @@ public class YPDrawSignatureView: UIView {
 	public var signatureBackgroundColor: UIColor = UIColor.whiteColor()
 	
 	// MARK: - Private properties
-	private var segments: [StrokeSegment] = []
-	private var currentSegment: StrokeSegment?
 	
 	private var points = [CGPoint](count: 5, repeatedValue: CGPoint())
 	private var pointCounter = 0
+	
+	private var signature = Signature()
 	
 	// MARK: - Init
 	required public init?(coder aDecoder: NSCoder) {
@@ -43,60 +42,35 @@ public class YPDrawSignatureView: UIView {
 	// MARK: - Draw
 	override public func drawRect(rect: CGRect) {
 		
-		for oneSegment in segments {
+		for oneSegment in signature.pathsForViewOfSize(self.frame.size) {
 			oneSegment.color.setStroke()
 			oneSegment.path.stroke()
 		}
 	}
 	
+	
 	// MARK: - Touch handling functions
 	override public func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-		if let firstTouch = touches.first{
-			let touchPoint = firstTouch.locationInView(self)
-			self.pointCounter = 0
-			self.points[0] = touchPoint
-			
-			self.currentSegment = StrokeSegment(path: UIBezierPath(), color: self.strokeColor, lineWidth:self.lineWidth)
-			segments.append(self.currentSegment!)
-		}
+		signature.startNewSegment(self.strokeColor, lineWidth:self.lineWidth)
+		signature.addPoint(touches.first!.locationInView(self), sourceViewSize: self.frame.size)
+		self.setNeedsDisplay()
 	}
 	
 	override public func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-		if let firstTouch = touches.first{
-			let touchPoint = firstTouch.locationInView(self)
-	  self.pointCounter += 1
-			self.points[self.pointCounter] = touchPoint
-			if (self.pointCounter == 4) {
-				self.points[3] = CGPointMake((self.points[2].x + self.points[4].x)/2.0, (self.points[2].y + self.points[4].y)/2.0)
-				self.segments.last!.path.moveToPoint(self.points[0])
-				self.segments.last!.path.addCurveToPoint(self.points[3], controlPoint1:self.points[1], controlPoint2:self.points[2])
-				
-				self.setNeedsDisplay()
-				self.points[0] = self.points[3]
-				self.points[1] = self.points[4]
-				self.pointCounter = 1
-			}
-			
-			self.setNeedsDisplay()
-		}
+		signature.addPoint(touches.first!.locationInView(self), sourceViewSize: self.frame.size)
+		self.setNeedsDisplay()
 	}
 	
 	override public func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-		if self.pointCounter == 0{
-			let touchPoint = self.points[0]
-			self.segments.last!.path.moveToPoint(CGPointMake(touchPoint.x-1.0,touchPoint.y))
-			self.segments.last!.path.addLineToPoint(CGPointMake(touchPoint.x+1.0,touchPoint.y))
-			self.setNeedsDisplay()
-		} else {
-			self.pointCounter = 0
-		}
+		signature.addPoint(touches.first!.locationInView(self), sourceViewSize: self.frame.size)
+		self.setNeedsDisplay()
 	}
 	
 	// MARK: - Helpers
 	
 	// MARK: Clear the Signature View
 	public func clearSignature() {
-		self.segments.removeAll()
+		self.signature.clearSignature()
 		self.setNeedsDisplay()
 	}
 	
